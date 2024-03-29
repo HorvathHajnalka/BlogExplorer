b89e1d42f5ba6206e2299a80086d180baba61234
Horvath Hajnalka <h.hajni2003@gmail.com>
2024-03-29 22:55:17 +0100

    try-client-server-db-conn-error

---
+++ BlogExplorer/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogExplorer.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+
+
+    }
+}
---
+++ BlogExplorer/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogExplorer
+{
+    public class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+    }
+}
