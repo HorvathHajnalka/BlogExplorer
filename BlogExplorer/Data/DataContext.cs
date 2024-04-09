437a18200694607669bb8eab82424cdd3504058b
erdosmarcell <erdosmarcell03@gmail.com>
2024-04-10 00:59:35 +0200

    database_fixed2

--- BlogExplorer/Data/DataContext.cs
+++ BlogExplorer/Data/DataContext.cs
@@ -6,11 +6,9 @@ namespace BlogExplorer.Data
 {
     public class DataContext : DbContext
     {
-        private readonly IConfiguration _configuration;
-
-        public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            _configuration = configuration;
+            Database.EnsureCreated();
         }
 
         public DbSet<User> Users { get; set; }
@@ -23,43 +21,9 @@ namespace BlogExplorer.Data
         {
             modelBuilder.Entity<FavoriteTopic>().HasKey(ft => new { ft.UserId, ft.TopicId });
 
-            // Seed initial data
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Username = "example_user", Name = "Example User", Password = "example_password" }
-                // Add additional users here
-            );
-
             base.OnModelCreating(modelBuilder);
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
-            }
-        }
-
-        public void EnsureDatabaseSeeded()
-        {
-            if (!Users.Any())
-            {
-                SeedUsers();
-            }
-
-            // További ellenőrzések és adatfeltöltések más entitásokhoz
-        }
-
-        private void SeedUsers()
-        {
-            var users = new List<User>
-            {
-                new User { Id = 1, Username = "example_user", Name = "Example User", Password = "example_password" }
-                // További felhasználók hozzáadása
-            };
-
-            Users.AddRange(users);
-            SaveChanges();
-        }
+       
     }
 }
