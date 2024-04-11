8a5551b68436d1071f0542fc24e34e092b6e3838
Horvath Hajnalka <h.hajni2003@gmail.com>
2024-04-11 15:41:43 +0200

    add-documentation

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShowTopicComponent } from './topic/show-topic/show-topic.component';
-import { HttpClientModule } from '@angular/common/http'; 
-
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { Title } from '@angular/platform-browser'; // Import the Title service
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ShowTopicComponent, HttpClientModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', // The component's CSS selector
+  standalone: true, // Marks the component as standalone
+  imports: [RouterOutlet, ShowTopicComponent, HttpClientModule], // Imports necessary modules and components
+  templateUrl: './app.component.html', // The location of the component's template file
+  styleUrls: ['./app.component.css'] // The location of the component's private CSS styles
 })
-export class AppComponent {
-  title = 'angular17-blogexplorer-api';
+export class AppComponent implements OnInit {
+  title = 'angular17-blogexplorer-api'; // Property for the application's title
+
+  constructor(private titleService: Title) { } // Inject the Title service
+
+  ngOnInit() {
+    this.titleService.setTitle('BlogExplorer'); // Set the browser tab title
+  }
 }
+
--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -1,19 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System.Linq;
 
 namespace BlogExplorer.Data
 {
+    // DataContext class inherits from DbContext, providing the functionalities for using Entity Framework Core.
     public class DataContext : DbContext
     {
+        // for configuration settings
         private readonly IConfiguration _configuration;
 
         public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options)
         {
+            // Ensures that the database for the context exists. If it exists, no action is taken.
+            // If it does not exist, then the database and all its schema are created.
             Database.EnsureCreated();
             _configuration = configuration;
         }
 
+        // Properties for each table in the database represented by a DbSet.
         public DbSet<User> Users { get; set; }
         public DbSet<Topic> Topics { get; set; }
         public DbSet<FavoriteTopic> FavoriteTopics { get; set; }
@@ -22,45 +26,21 @@ namespace BlogExplorer.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Configures a composite primary key for the FavoriteTopic entity.
             modelBuilder.Entity<FavoriteTopic>().HasKey(ft => new { ft.UserId, ft.TopicId });
 
-            // Seed initial data
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Username = "example_user", Name = "Example User", Password = "example_password" }
-                // Add additional users here
-            );
+            // Further configurations and seed data additions can be done here.
 
             base.OnModelCreating(modelBuilder);
         }
 
+        // Configures the context to connect to a SQL Server database using the connection string
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
             }
-        }
-
-        public void EnsureDatabaseSeeded()
-        {
-            if (!Users.Any())
-            {
-                SeedUsers();
-            }
-
-            // További ellenőrzések és adatfeltöltések más entitásokhoz
-        }
-
-        private void SeedUsers()
-        {
-            var users = new List<User>
-            {
-                new User { Id = 1, Username = "example_user", Name = "Example User", Password = "example_password" }
-                // További felhasználók hozzáadása
-            };
-
-            Users.AddRange(users);
-            SaveChanges();
         }
     }
 }
