9022ca9e15cab4fa081c0746453ba25b299577b7
Horvath Hajnalka <h.hajni2003@gmail.com>
2024-04-13 14:29:42 +0200

    routing-works

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserComponent } from './user/user.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/main-page', pathMatch: 'full' },
     {path:'topics', component:TopicComponent},
     {path:'users', component:UserComponent},
     {path:'main-page', component:MainPageComponent}
