8284699e17a18111dad35ec9d8d54c7b2e2e1531
Horvath Hajnalka <h.hajni2003@gmail.com>
2024-04-13 10:13:33 +0200

    routing-settings

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 // Import services
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ShowTopicComponent } from './topic/show-topic/show-topic.component';
 import { TopicComponent } from './topic/topic.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root', // The component's CSS selector
   standalone: true, // Marks the component as standalone
-  imports: [RouterOutlet, ShowTopicComponent, TopicComponent, HttpClientModule], // Imports necessary modules and components
+  imports: [RouterOutlet, ShowTopicComponent, TopicComponent, HttpClientModule, RouterLink, RouterLinkActive], // Imports necessary modules and components
   templateUrl: './app.component.html', // The location of the component's template file
   styleUrls: ['./app.component.css'] // The location of the component's private CSS styles
 })
--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TopicComponent } from './topic/topic.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'topics', component:TopicComponent}
+]
+
