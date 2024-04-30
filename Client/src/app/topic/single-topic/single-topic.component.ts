cdd9f868e8a8bf0c741b89fea5f26feef16ca2d9
HorvJudit <151916917+HorvJudit@users.noreply.github.com>
2024-04-30 17:58:01 +0200

    update single-topic

--- Client/src/app/topic/single-topic/single-topic.component.ts
+++ Client/src/app/topic/single-topic/single-topic.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BlogApiService } from '../../services/blog-api.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-topic',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-topic.component.html',
   styleUrl: './single-topic.component.css'
 })
