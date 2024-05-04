088ae4c3a99cf78b1f9514cac584b82fb1185a29
HorvJudit <151916917+HorvJudit@users.noreply.github.com>
2024-05-04 16:21:58 +0200

    connect fav chechbox to database

--- Client/src/app/topic/single-topic/single-topic.component.ts
+++ Client/src/app/topic/single-topic/single-topic.component.ts
@@ -1,106 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Az FormsModule importálása
 import { BlogApiService } from '../../services/blog-api.service';
 import { UserStoreService } from '../../services/user-store.service';
 import { AuthService } from '../../services/auth.service';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-single-topic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './single-topic.component.html',
   styleUrl: './single-topic.component.css'
 })
-export class SingleTopicComponent {
+export class SingleTopicComponent implements OnInit{
+  
+  userId:any = [];
   topicId!: number;
-  //userId!: number;
-  userId! : Observable<string>
   topic: any = {};
-  commentList$: Observable<any[]> | undefined 
-  isFavourite: boolean = false;
+  commentList$: Observable<any[]> | undefined   
   newcomment: any;
-  checkbox = document.getElementById('fav-checkbox') as HTMLElement;
+  isChecked: boolean | undefined; 
   
 
-  constructor(private route: ActivatedRoute, private apiservice: BlogApiService, private userstoreservice: UserStoreService, private authsetvice: AuthService) {}
+  constructor(private route: ActivatedRoute, private apiservice: BlogApiService, private userstoreservice: UserStoreService, private authservice: AuthService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.topicId = params['id'];
-      this.userId = this.userstoreservice.getUserIdFromStore();
-      /*this.userstoreservice.getUserIdFromStore().pipe(
-        map(userId => parseInt(userId, 10))
-      ).subscribe(
-        userId => {
-          this.userId = userId;
-        },
-        error => {
-          console.error('Hiba történt:', error);
-        }
-      );*/
-      this.loadTopic();  
-      this.getComments();  
-    });   
-    
-    this.checkFavourite();
+
+      this.topicId = params['id'];           
+      
+    });       
+
+    this.userstoreservice.getUserIdFromStore()
+      .subscribe(val=>{
+        let userIdFromToken = this.authservice.getUserIdFromToken();
+        this.userId = val || userIdFromToken
+    })
+
+    this.apiservice.getFavTopic(this.userId, this.topicId)
+  .pipe(
+    catchError(error => {
+      if (error.status === 404) {
+        this.isChecked = false;
+        return of(null);
+      } else {        
+        console.error('unknown error: ', error);
+        return EMPTY;
+      }
+    })
+  )
+  .subscribe(response => {
+    this.isChecked = response !== null;
+  });
+
+    this.loadTopic();  
+    this.getComments();  
     
   }
 
+  onCheckboxChange(): void {
+  
+    if (this.isChecked) {
 
+      console.log("The chechbox is checked");
+
+      var data = {
+        "userId": this.userId,
+        "topicId": this.topicId
+      }
+
+      this.apiservice.addFavTopic(data).subscribe(response => {
+        this.openSnackBar(`${this.topic.name} successfully added to favourite topics`);
+      }, error => {
+        this.openSnackBar('Something went wrong: '+ error);
+      });
+    } else {
+
+      console.log('The chechbox is unchecked');
+
+      this.apiservice.deleteFavTopic(this.userId, this.topicId).subscribe(response => {
+        this.openSnackBar(`${this.topic.name} successfully deleted from favourite topics`);
+      }, error => {
+        this.openSnackBar('Something went wrong: '+ error);
+      });
+    }
+  }
+
+  //this function loads the clicked topic
   loadTopic() {
     this.apiservice.getTopic(this.topicId).subscribe(details => {
       this.topic = details;
     });    
   }
 
-  /*getComments(): void {
-    this.commentList$ = this.apiservice.getCommentList()
-  }*/
-
-  getComments(): void {
-    
+  //this function gets the list of all comments, and filters it to the current topic
+  getComments(): void {    
     this.commentList$ = this.apiservice.getCommentList().pipe(
         map(comments => comments.filter(comment => comment.topicId == this.topicId))
     );    
-}
-
-  //getComments(): void {
-  //  this.service.getCommentList().subscribe(
-  //    (data: any[]) => {
-  //      console.log('Comments:', data);
-  //      this.commentList$ = of(data.filter(comment => comment.topicId === this.topicId));
-  //    },
-  //    error => {
-  //      console.error('Error fetching comments:', error);
-  //    }
-  //  );
-  //}
-
-  checkFavourite(): void {
-    console.log(`userId: ${this.userId}`);
   }
 
-  toggleFavourite(): void {
-    if (this.checkbox){
-      
-    }
-    // Implementálj egy hívást, ami frissíti az adatbázist attól függően, hogy a téma kedvenc-e vagy sem
-    // Ehhez használhatsz egy olyan függvényt, ami az API segítségével beállítja vagy törli a kedvenc témát
-    // Példaként a checkFavorite() függvényt használjuk, hogy beállítsuk vagy töröljük a kedvenc témát
-
-    /*
-
-    //ez így nem jó, de tudnom kell hozzá, hogy működik az API
-    this.service.setFavoriteTopic(this.topicId, !this.isFavourite).subscribe(() => {
-      this.isFavourite = !this.isFavourite;
-    });*/
+  openSnackBar(message: string, action: string = '') {
+    this.snackBar.open(message, action, {
+      duration: 2000, // A snackbar megjelenési ideje milliszekundumban
+    });
   }
 }
 
